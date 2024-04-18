dc17afce66945179bf6f41d21de891f75db811c1
olusprogr <olus.main333@gmail.com>
2024-04-18 16:53:16 +0200

    added hashlocation strategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { provideRouter, withHashLocation } from '@angular/router';
+import { provideClientHydration } from '@angular/platform-browser';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideRouter(routes, withHashLocation())
+  ]
 };
